182407244ea6b87259d046159bf5f488e3bde649
daniel-butler <dabutler89@gmail.com>
2021-12-01 16:51:04 -0500

    getting base level to work

--- alexa.py
+++ alexa.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from ask_sdk_core import utils as ask_utils
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_core.utils import is_intent_name, is_request_type
@@ -43,10 +46,11 @@ def last_feeding_intent_handler(handler_input: HandlerInput) -> Response:
 
 @sb.request_handler(can_handle_func=is_intent_name("NewBabyIntent"))
 def add_new_baby(handler_input: HandlerInput) -> Response:
-    session_attributes = handler_input.attributes_manager.session_attributes
+    first_name = ask_utils.get_slot(handler_input, 'first_name')
+    birthday = ask_utils.get_slot(handler_input, 'birthday')
     speech, ask = actions.new_baby(
-        baby_name=session_attributes.get('first_name'),
-        birthday=session_attributes.get('birthday'),
+        baby_name=first_name.value if first_name else None,
+        birthday=datetime.strptime(birthday.value, '%Y-%m%d').date() if birthday else None,
     )
     handler_input.response_builder\
         .speak(speech)\
